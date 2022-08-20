c441f8510610d6706cd529086a8990937e8c042c
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2022-08-20 19:46:25 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const input = document.querySelector("input");
+
+const qr = document.querySelector(".qr");
+const qrbox = document.querySelector("#qrbox");
+const status = document.querySelector(".status");
+
+const buttons = document.querySelector(".buttons");
+
+function generateQRCode(text) {
+  const qrcode = new QRCode("qrbox", {
+    text: text,
+    width: 300,
+    height: 300,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+}
+
+function download(url) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = url.split("/").pop();
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+function submitHandler() {
+  if (input.value.length > 0) {
+    qrbox.innerHTML = "";
+    buttons.style.display = "none";
+
+    qr.style.display = "block";
+    status.style.display = "block";
+
+    setTimeout(() => {
+      status.style.display = "none";
+
+      generateQRCode(input.value);
+
+      const image = qrbox.querySelector("img");
+      document.querySelector(".download").onclick = () => {
+        download(image.src);
+      };
+
+      qrbox.style.display = "block";
+      buttons.style.display = "flex";
+    }, 1000);
+  } else {
+    alert("Input field is empty!");
+  }
+}
+
+function deleteHandler() {
+  qrbox.innerHTML = "";
+  buttons.style.display = "none";
+  qr.style.display = "none";
+}
